29a68c71aab986280a208cbbaea88a102ee68923
khaled Mohammed <khaled.freedom2016@gmail.com>
2021-12-19 02:58:59 +0200

    add running.sh

---
+++ running.sh
@@ -0,0 +1,17 @@
+
+##################shell command ############$$$$$$$$$$$$
+echo 'Love Linux'
+
+git clone https://github.com/KhaledAbd/Asara.git
+cd Asara/Asara-APP/ ## Redirect to Folder 
+npm install
+ng serve
+
+
+=======================================>>>>>>>>>>>>>>>>>>>> Open Second Terminal And Return To main Path<<<<<< ============================================================
+cd ../Asara.API/
+dotnet restore ### for Restore Package
+dotnet watch run
+
+
+
